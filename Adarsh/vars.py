e8b01a1d5b6c3c99f92dc2fccf66ae10cc48ec42
BrhamaKumar <146445615+BrhamaKumar@users.noreply.github.com>
2024-01-21 13:48:01 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -8,20 +8,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
-    name = str(getenv('name', 'Nobita-Stream-Bot'))
+    API_ID = int(getenv('API_ID', '20810825'))
+    API_HASH = str(getenv('API_HASH', '707e67f53b4593a3e9b6b424311f84d0'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6602440459:AAEKnqKvIJDEs6Qb4qRpAX8bsqk2579EqKw'))
+    name = str(getenv('name', 'File_To_Link_Url_to_Link_Bot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002133784154'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", ""))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6301693754").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'NobiDeveloperr'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Pukhraj_Prajapat_001'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -34,9 +34,9 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
-    SHORTLINK_URL = getenv('SHORTLINK_URL', 'tnshort.net')
-    SHORTLINK_API = getenv('SHORTLINK_API', '0c8ebd63bfe9f67f9970b8767498ff60316b9b03')
-    TUTORIAL_URL = getenv('TUTORIAL_URL', 'https://youtu.be/rddlpYLm0G0')    
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://gitadevidevi053:Brhama025@cluster0.9rj57mi.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'demosaveforce'))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))
+    SHORTLINK_URL = getenv('SHORTLINK_URL', 'moneycase.link')
+    SHORTLINK_API = getenv('SHORTLINK_API', '95fc96273f2174408c7126b8b55619171e12bfd8')
+    TUTORIAL_URL = getenv('TUTORIAL_URL', 'https://t.me/+C7-_1fuc7zViOTk1')    
